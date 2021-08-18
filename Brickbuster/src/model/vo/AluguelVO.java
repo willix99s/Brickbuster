fbe5be7cf4292c450d36e5523138e36603463a60
willix99s <william-praxedes@hotmail.com>
2021-08-18 15:12:03 -0300

    Adicionamos todos as classes

---
+++ Brickbuster/src/model/vo/AluguelVO.java
@@ -0,0 +1,62 @@
+package model.vo;
+import java.util.*;
+
+public class AluguelVO {
+	//Atributos de aluguel
+	private ClientesVO nome;
+	private Calendar dataInicio;
+	private Calendar dataFinal;
+	private double valorAluguel;
+	private DiscoVO disco[];
+	private LivroVO livro[];
+	
+	//Get e Set de nome
+	public ClientesVO getNome() {
+		return nome;
+	}
+	public void setNome(ClientesVO nome) {
+		this.nome = nome;
+	}
+	
+	//Get e Set de data inicial
+	public Calendar getDataInicio() {
+		return dataInicio;
+	}
+	public void setDataInicio(Calendar dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+	
+	//Get e Set de data final
+	public Calendar getDataFinal() {
+		return dataFinal;
+	}
+	public void setDataFinal(Calendar dataFinal) {
+		this.dataFinal = dataFinal;
+	}
+	
+	//Get e Set de valor do aluguel
+	public double getValorAluguel() {
+		return valorAluguel;
+	}
+	public void setValorAluguel(double valorAluguel) {
+		this.valorAluguel = valorAluguel;
+	}
+	
+	//Get e Set de disco
+	public DiscoVO[] getDisco() {
+		return disco;
+	}
+	public void setDisco(DiscoVO[] disco) {
+		this.disco = disco;
+	}
+	
+	//Get e Set de livro
+	public LivroVO[] getLivro() {
+		return livro;
+	}
+	public void setLivro(LivroVO[] livro) {
+		this.livro = livro;
+	}
+	
+	
+}
