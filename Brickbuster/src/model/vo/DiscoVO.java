e0ed0037026497dab6e58805c60805d610869ab7
Paulo Victor <paulovictorbema@gmail.com>
2021-08-19 14:52:32 -0300

    Fiz a classe Livro e realizei algumas mudanças em Clientes e Disco

--- Brickbuster/src/model/vo/DiscoVO.java
+++ Brickbuster/src/model/vo/DiscoVO.java
@@ -3,7 +3,7 @@ package model.vo;
 public class DiscoVO {
 	//Atributos de DiscoVO
 	private String titulo;
-	private String nome;
+	private String banda;
 	private String estilo;
 	private int exemplares;
 	private double valorAluguel;
@@ -13,25 +13,25 @@ public class DiscoVO {
 		return titulo;
 	}
 	public void setTitulo(String titulo) {
-		if (titulo==null) {System.out.println ("Digite um Título válido");}
+		if (titulo==null) {System.out.println ("Digite um título válido");}
 		else 
 		{
-			if (nome.equals(" ") ) {System.out.println ("Digite um Título válido");}
+			if (titulo.equals(" ") ) {System.out.println ("Digite um título válido");}
 			else {this.titulo = titulo;}
 		}
 		
 	}
 	
-	//Get e Set de nome
-	public String getNome() {
-		return nome;
+	//Get e Set de banda
+	public String getBanda() {
+		return banda;
 	}
-	public void setNome(String nome) {
-		if (nome==null) {System.out.println ("Digite um nome válido");}
+	public void setBanda(String banda) {
+		if (banda==null) {System.out.println ("Digite uma banda válida");}
 		else
 		{
-			if (nome.equals(" ") ) {System.out.println ("Digite um nome Válido");}
-			else {this.nome = nome;}
+			if (banda.equals(" ") ) {System.out.println ("Digite uma banda válida");}
+			else {this.banda = banda;}
 		}
 		
 	}
@@ -41,10 +41,10 @@ public class DiscoVO {
 		return estilo;
 	}
 	public void setEstilo(String estilo) {
-		if (estilo==null) {System.out.println ("Digite um nome válido");}
+		if (estilo==null) {System.out.println ("Digite um estilo válido");}
 		else 
 		{
-			if (nome.equals(" ") ) {System.out.println ("Digite um nome Válido");}
+			if (estilo.equals(" ") ) {System.out.println ("Digite um estilo válido");}
 			else {this.estilo = estilo;}
 		}
 		
@@ -65,7 +65,7 @@ public class DiscoVO {
 		return valorAluguel;
 	}
 	public void setValorAluguel(double valorAluguel) {
-		if (valorAluguel <= 0) {System.out.println ("Valor não permitido ");}
+		if (valorAluguel <= 0) {System.out.println ("Valor não permitido");}
 		else
 		this.valorAluguel = valorAluguel;
 	}
