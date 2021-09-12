954fe04fca163e838fb9b5db6886280fa8063266
Paulo Victor <paulovictorbema@gmail.com>
2021-09-12 17:11:51 -0300

    Colocando o AdminDAO

--- Brickbuster/src/model/dao/AdminDAO.java
+++ Brickbuster/src/model/dao/AdminDAO.java
@@ -1,5 +1,84 @@
 package model.dao;
 
-public class AdminDAO {
-//teste
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.vo.AdminVO;
+
+public class AdminDAO extends BaseDAO {
+	
+	public void inserir(AdminVO vo) {
+		conn = getConnection();
+		String sql = "insert into Admin (codAdmin, login, senha, nome) values (?,?,?,?)";
+		PreparedStatement ptst;
+		try {
+			ptst = conn.prepareStatement(sql);
+			ptst.setInt(1, vo.getCodAdmin());
+			ptst.setString(2, vo.getLogin());
+			ptst.setString(3, vo.getSenha());
+			ptst.setString(4, vo.getNome());
+			ptst.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void remover(AdminVO vo) {
+		conn = getConnection();
+		String sql = "delete from Admin where codAdmin = ?";
+		PreparedStatement ptst;
+		try {
+			ptst = conn.prepareStatement(sql);
+			ptst.setInt(1, vo.getCodAdmin());
+			ptst.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public List<AdminVO> listar(){
+		conn = getConnection();
+		String sql = "select * from Admin";
+		Statement st;
+		ResultSet rs;
+		List<AdminVO> admins = new ArrayList<AdminVO>();
+		try {
+			st = conn.createStatement();
+			rs = st.executeQuery(sql);
+			while(rs.next()) {
+				AdminVO vo = new AdminVO();
+				vo.setCodAdmin(rs.getInt("codAdmin"));
+				vo.setLogin(rs.getString("login"));
+				vo.setNome(rs.getString("nome"));
+				vo.setSenha(rs.getString("senha"));
+				admins.add(vo);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return admins;
+	}
+	
+	public void editar(AdminVO vo) {
+		conn = getConnection();
+		String sql = "update Admin set nome = ? where codAdmin = ?";
+		PreparedStatement ptst;
+		try {
+			ptst = conn.prepareStatement(sql);
+			ptst.setInt(1, vo.getCodAdmin());
+			ptst.setString(2, vo.getNome());
+			ptst.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
