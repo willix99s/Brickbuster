4000bf79d0448526ddd916f917c36081e2513cf5
willix99s <william-praxedes@hotmail.com>
2021-10-16 14:40:18 -0300

    Adição dos códigos restantes

---
+++ Brickbuster/src/view/TelaRelatorio.java
@@ -0,0 +1,28 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TelaRelatorio extends Application{
+
+	public static void main(String[] args) {
+		launch();
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("VE/Relatorio.fxml"));
+		
+		Scene cena = new Scene(root);
+		
+		primaryStage.setTitle("Brickbuster");
+		primaryStage.setScene(cena);
+		primaryStage.show();
+		
+	}
+
+}
